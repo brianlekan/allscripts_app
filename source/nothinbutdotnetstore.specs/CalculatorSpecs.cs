b3c48e85aa09e8f3d600f473e4b9721bdde55189
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-09-14 14:16:19 -0600

    Updated

--- source/nothinbutdotnetstore.specs/CalculatorSpecs.cs
+++ source/nothinbutdotnetstore.specs/CalculatorSpecs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using Machine.Specifications;
 using developwithpassion.specifications.rhinomocks;
 using developwithpassion.specifications.extensions;
@@ -14,13 +15,22 @@ namespace nothinbutdotnetstore.specs
 
         public class when_adding_two_positive_numbers : concern
         {
+            Establish c = () =>
+            {
+                connection = depends.on<IDbConnection>();
+            };
+
             Because b = () =>
                 result = sut.add(2, 3);
 
             It should_return_the_sum = () =>
                 result.ShouldEqual(5);
 
+            It should_open_a_connection_to_the_database = () =>
+                connection.received(x => x.Open());
+
             static int result;
+            static IDbConnection connection;
         }
 
         public class when_attempting_to_add_a_negative_number : concern
--- source/nothinbutdotnetstore/Calculator.cs
+++ source/nothinbutdotnetstore/Calculator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Linq;
 
 namespace nothinbutdotnetstore
 {
@@ -9,13 +11,25 @@ namespace nothinbutdotnetstore
 
     public class Calculator : ICalculate
     {
+        IDbConnection connection;
+
+        public Calculator(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+
         public int add(int first, int second)
         {
-            if (first <=0 || second <=0 )
-            {
-                throw new ArgumentException();
-            }
+            ensure_all_are_positive(first, second);
+
             return first + second;
+        }
+
+        void ensure_all_are_positive(params int[] numbers)
+        {
+            if (numbers.All(x => x > 0)) return;
+
+            throw new ArgumentException();
         }
     }
 }
